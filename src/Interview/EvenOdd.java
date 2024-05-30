7371bd1f8b0e3705e7d6a0b6589c0bc99bd2330b
Omkar-Nilakhe <nilakheomkar@gmail.com>
2024-05-30 15:49:24 +0530

    new program added

---
+++ src/Interview/EvenOdd.java
@@ -0,0 +1,22 @@
+package Interview;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number to check even or odd :");
+		int num=sc.nextInt();
+		
+		if(num%2==0) {
+			System.out.println(num+" Number is Even");
+			
+		}else {
+			System.out.println(num+" Number is Odd");
+		}
+
+	}
+
+}
